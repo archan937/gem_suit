a70d65bff72c0c1d25177e5ea74b956d9325df4e
Paul Engel <paul.engel@holder.nl>
2011-03-31 00:08:27 +0200

    Improved gem lib requirement within test_helper.rb (being able to print error when loading)

--- templates/dynamic/suit/shared/test/test_helper.rb
+++ templates/dynamic/suit/shared/test/test_helper.rb
@@ -10,6 +10,10 @@ require "mocha"
 
 begin
   require File.expand_path("../../../../lib/<%= gem_name %>", __FILE__)
-rescue LoadError
-  require File.expand_path("../../../../../lib/<%= gem_name %>", __FILE__)
+rescue LoadError => e
+  begin
+    require File.expand_path("../../../../../lib/<%= gem_name %>", __FILE__)
+  rescue LoadError
+    puts "ERROR: #{e.message}\n\n"
+  end
 end
